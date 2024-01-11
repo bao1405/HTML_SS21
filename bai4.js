af5414566f71efcc075bed19ad2f5de41199303d
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-11 16:30:12 +0700

    Update bai4.js

--- bai4.js
+++ bai4.js
@@ -1,6 +1,6 @@
-var soNhap = prompt("Nhập vào một số:");
-var soNguyen = parseFloat(soNhap);
-var ketQua;
+let soNhap = prompt("Nhập vào một số:");
+let soNguyen = parseFloat(soNhap);
+let ketQua;
 if (soNguyen > 0) {
     ketQua = "Số dương";
 } else if (soNguyen < 0) {
