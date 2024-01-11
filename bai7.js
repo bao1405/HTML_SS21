6ce8dd57a3ccd1d07fe78fa1e71ae1fc251c7270
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-11 16:31:25 +0700

    Update bai7.js

--- bai7.js
+++ bai7.js
@@ -1,6 +1,6 @@
-var a = parseFloat(prompt("Nhập số a:"));
-var b = parseFloat(prompt("Nhập số b:"));
-var c = parseFloat(prompt("Nhập số c:"));
+let a = parseFloat(prompt("Nhập số a:"));
+let b = parseFloat(prompt("Nhập số b:"));
+let c = parseFloat(prompt("Nhập số c:"));
 
-var soLonNhat = Math.max(a, b, c);
+let soLonNhat = Math.max(a, b, c);
 console.log("Số lớn nhất là: " + soLonNhat);
