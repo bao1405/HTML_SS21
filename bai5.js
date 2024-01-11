7bd2eeb263b24b1dde55bb2e2b58e832abd3abd0
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-11 16:30:28 +0700

    Update bai5.js

--- bai5.js
+++ bai5.js
@@ -1,5 +1,5 @@
-var thangNhap = prompt("Nhập vào số tháng trong năm (từ 1 đến 12):");
-var thang = parseInt(thangNhap);
+let thangNhap = prompt("Nhập vào số tháng trong năm (từ 1 đến 12):");
+let thang = parseInt(thangNhap);
 if (thang >= 1 && thang <= 12) {
     switch (thang) {
         case 1: case 3: case 5: case 7: case 8: case 10: case 12:
