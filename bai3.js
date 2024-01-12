73620076b46354aaf25554d192b4eb130a6af5f8
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-12 11:52:12 +0700

    Update bai3.js

--- bai3.js
+++ bai3.js
@@ -1,5 +1,5 @@
-let soNhap = prompt("Nhập vào một số từ 2 đến 8:");
-let soNguyen = parseInt(soNhap);
+var soNhap = prompt("Nhập vào một số từ 2 đến 8:");
+var soNguyen = parseInt(soNhap);
 switch (soNguyen) {
     case 2:
         console.log("Monday");
